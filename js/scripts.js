4f73650964cddaf4ad9602378daf3e1f53282549
VictorKMaina <vk13runic@gmail.com>
2020-02-23 18:26:23 +0300

    correct JS indentation

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,13 @@ $().ready(function () {
   $("#design").click(function () {
     $("#design-1").toggle();
     $("#design-2").toggle();
-  })
+  });
   $("#develop").click(function () {
     $("#develop-1").toggle();
     $("#develop-2").toggle();
-  })
+  });
   $("#product").click(function () {
     $("#product-1").toggle();
     $("#product-2").toggle();
-  })
+  });
 })
